0bd71e21c5bad9faa1e43785767edb18dc697ec1
tabbylie <dyoung8765@gmail.com>
2020-05-15 00:15:55 -0400

    fixed error

--- app/models.py
+++ app/models.py
@@ -36,7 +36,7 @@ class Orders(db.Model):
 	order_name = db.Column(db.String(64), index=True)
 	order_desc = db.Column(db.String(128), index=True, unique=True)
 	time_ordered = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-	order_flag = db.Column(db.String(32)m index=True)
+	order_flag = db.Column(db.String(32), index=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 	def __repr__(self):
