168bddbfaffa0754d3a37483435620b57264ecea
tabbylie <dyoung8765@gmail.com>
2020-07-29 21:44:35 -0400

    fixing stuff

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -1,10 +1,3 @@
-var onImg = "https://image.flaticon.com/icons/svg/149/149176.svg";
-var offImg = "https://image.flaticon.com/icons/svg/149/149147.svg";
-trans = () => {
-    var img = document.getElementById("menu-icon");
-    img.src = img.src == offImg ? onImg : offImg;
-}
-
 const supportForm = document.querySelector("#support-form");
 openSupportForm = () => {
     supportForm.style.display = "block";
